870e5d148c7a1bd961ac75d3d6ef0fb1c71f8864
OREShake <nikita3743b@gmail.com>
2024-06-23 23:18:07 +0200

    Add vtk

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageLayout
 from trame.widgets import vuetify, vtk as vtk_widgets
+from vtkmodules.all import *
 
 server = get_server()
 server.client_type = "vue2"
 state, ctrl = server.state, server.controller
+
+cone = vtkConeSource()
+cone.Update()
 
 state.trame__title = "Trame Sandbox"
 
