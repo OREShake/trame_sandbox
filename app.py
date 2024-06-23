d2acf05cf2197879312595effe58bd4f037036e1
OREShake <nikita3743b@gmail.com>
2024-06-23 23:44:08 +0200

    Test vtk

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageLayout
 from trame.widgets import vuetify, vtk as vtk_widgets
-# from vtkmodules.all import *
+from vtkmodules.all import *
 
 server = get_server()
 server.client_type = "vue2"
