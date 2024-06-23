e74e2dc9318c7fc5a0433db36bb17dad46c624b6
OREShake <nikita3743b@gmail.com>
2024-06-23 23:41:13 +0200

    Fix vtk

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageLayout
 from trame.widgets import vuetify, vtk as vtk_widgets
-from vtkmodules.all import *
+# from vtkmodules.all import *
 
 server = get_server()
 server.client_type = "vue2"
 state, ctrl = server.state, server.controller
 
-cone = vtkConeSource()
-cone.Update()
+# cone = vtkConeSource()
+# cone.Update()
 
-mapper = vtkPolyDataMapper()
-mapper.SetInputConnection(cone.GetOutputPort())
+# mapper = vtkPolyDataMapper()
+# mapper.SetInputConnection(cone.GetOutputPort())
 
-actor = vtkActor()
-actor.SetMapper(mapper)
+# actor = vtkActor()
+# actor.SetMapper(mapper)
 
 state.trame__title = "Trame Sandbox"
 
