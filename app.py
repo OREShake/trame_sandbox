4fc2322f4d424308fe55830eef111f6c5ebdeaee
OREShake <nikita3743b@gmail.com>
2024-06-24 07:18:46 +0200

    Change view type

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ with SinglePageLayout(server) as layout:
         with vuetify.VContainer(
                 fluid=True,
                 classes="pa-0 fill-height",):
-            view = vtk.VtkRemoteView(
+            view = vtk.VtkLocalView(
                 window, interactive_quality=100, interactive_ratio=1)
             ctrl.view_update = view.update
             ctrl.view_reset_camera = view.reset_camera
