0a45dbda61bdc5ca2160591fe3ba1b52c8f13d6b
OREShake <nikita3743b@gmail.com>
2024-06-25 14:58:39 +0200

    Fix error

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ reader.Update()
 data = vtkUnstructuredGrid()
 data.ShallowCopy(reader.GetOutput())
 
+grid = vtkUnstructuredGrid()
+grid.ShallowCopy(reader.GetOutput())
+
 variables = []
 for i in range(data.GetPointData().GetNumberOfArrays()):
     variables.append(data.GetPointData().GetArrayName(i))
@@ -45,12 +48,27 @@ data_actor.SetMapper(data_mapper)
 data_actor.GetProperty().SetAmbient(0.4)
 data_actor.GetProperty().SetDiffuse(1)
 
+grid_filter = vtkUnstructuredGridGeometryFilter()
+grid_filter.SetInputData(grid)
+
+grid_extracter = vtkExtractEdges()
+grid_extracter.SetInputConnection(grid_filter.GetOutputPort())
+
+grid_mapper = vtkDataSetMapper()
+grid_mapper.SetInputConnection(grid_extracter.GetOutputPort())
+
+grid_actor = vtkActor()
+grid_actor.SetMapper(grid_mapper)
+grid_actor.GetProperty().SetRepresentationToWireframe()
+grid_actor.GetProperty().SetColor(0, 0, 0)
+
 # -----------------------------------------------------------------------------
 # Visualization space
 # ----------------------------------------------------------------------------
 
 renderer = vtkRenderer()
 renderer.AddActor(data_actor)
+renderer.AddActor(grid_actor)
 renderer.SetBackground(1, 1, 1)
 renderer.ResetCamera()
 
